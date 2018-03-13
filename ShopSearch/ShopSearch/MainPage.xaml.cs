238305faab395b965c0ee837254b56cfc428f422
Ayman Gassara <a.gassara@root.sa>
2018-03-13 15:23:22 +0100

    Add project files.

---
+++ ShopSearch/ShopSearch/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ShopSearch
+{
+	public partial class MainPage : ContentPage
+	{
+        List<Products> Products = new List<Products>
+        {
+            new Products{ Name="Gucci Hand Bag Light Brown" , Category="Almost New", Price="3000.00 SAR" , Image="Bag1.png"},
+            new Products{ Name="Gucci Hand Bag Light Brown" , Category="Almost New", Price="3000.00 SAR" , Image="Bag2.png" },
+            new Products{ Name="Gucci Hand Bag Light Brown" , Category="Almost New", Price="3000.00 SAR" , Image="Bag3.png"}
+        };
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            NamesListView.ItemsSource = Products;
+
+        }
+
+        private void SearchBar_Presses(object sender , TextChangedEventArgs e)
+        {
+            var keyword = MainSearchbar.Text;
+            NamesListView.ItemsSource = Products.Where(Products => Products.Name.ToLower().Contains(keyword.ToLower()));
+        }
+
+
+    }
+}
---
+++ ShopSearch/ShopSearch/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopSearch
+{
+    public class Products
+    {
+        public string Name { get; set;}
+        public string Category { get; set; }
+        public string Price { get; set; }
+        public string Image { get; set; }
+
+
+
+    }
+}
